9b30a9f16ceb0f1737705b8beff23432fdd60b02
Amr Salama <amr.salama@benestudio.co>
2020-10-16 08:46:29 +0200

    IWS-80: feat: init examples

---
+++ examples/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Simple from './Simple';
+
+function App() {
+    return (
+        <div>
+            <h1>Scrollfade Examples</h1>
+            <Simple />
+        </div>
+    );
+}
+
+export default App;
