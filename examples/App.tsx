5b96e073113241c505c1e80f97ee3336edff05f0
Amr Salama <amr.salama@benestudio.co>
2020-11-06 10:52:03 +0100

    IWS-80: feat: use arrow function

--- examples/App.tsx
+++ examples/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Simple from './Simple';
 
-export const App = () => {
-    return (
-        <div>
-            <h1>Scrollfade Examples</h1>
-            <Simple />
-        </div>
-    );
-};
+export const App = () => (
+    <div>
+        <h1>Scrollfade Examples</h1>
+        <Simple />
+    </div>
+);
