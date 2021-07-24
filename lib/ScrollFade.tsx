27a72477f9287d0f0e07130ba3e6add4cf3acf1e
Abolarin Olanrewaju <waju.abolarin@gmail.com>
2021-07-24 17:41:05 +0100

    Add height props with a default

--- lib/ScrollFade.tsx
+++ lib/ScrollFade.tsx
@@ -1,18 +1,23 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 
 const easeIn = (t: number, alpha: number) => Math.pow(t, alpha);
+const FADE_HEIGHT = 56;
 
-const getMask = (opacity: number) => `linear-gradient(180deg, black, rgba(255, 255, 255, ${opacity})) center bottom/100%
-    56px no-repeat,
-linear-gradient(180deg, black, black) center top/100% calc(100% - 56px)
-    no-repeat`;
+type ScrollFadeProps = { height?: number };
 
-const ScrollFade = () => {
+const ScrollFade = ({ height }: ScrollFadeProps) => {
     const rootRef = useRef<HTMLDivElement>(null);
+    const getMaskRef = useRef<(opacity: number) => string>(
+        (opacity: number) => `linear-gradient(180deg, black, rgba(0,50, 255, ${opacity})) center bottom/100%
+        ${height}px no-repeat,
+    linear-gradient(180deg, black, black) center top/100% calc(100% - ${height}px)
+        no-repeat`
+    );
 
     const onScroll = useCallback(() => {
         const scrollElement = rootRef.current?.parentElement;
         if (scrollElement) {
+            const getMask = getMaskRef.current;
             const { offsetHeight: elementHeight, scrollHeight: elementWidth, scrollTop } = scrollElement;
             const opacity = easeIn(scrollTop / (elementHeight - elementWidth), 10);
             const mask = getMask(opacity);
@@ -28,7 +33,7 @@ const ScrollFade = () => {
         if (scrollElement) {
             const { offsetHeight, scrollHeight } = scrollElement;
             if (offsetHeight !== scrollHeight) {
-                const mask = getMask(0);
+                const mask = getMaskRef.current(0);
                 scrollElement.style.mask = mask;
                 scrollElement.style.webkitMask = mask;
             }
@@ -40,5 +45,7 @@ const ScrollFade = () => {
 
     return <div className="scroll-fade" ref={rootRef} />;
 };
-
+ScrollFade.defaultProps = {
+    height: FADE_HEIGHT,
+};
 export default ScrollFade;
