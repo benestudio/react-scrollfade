e708275ed01145308c483f83ca59efabd6697c49
Abolarin Olanrewaju <waju.abolarin@gmail.com>
2021-07-24 17:41:18 +0100

    add example and documentation

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# React Scrollfade #
+# React Scrollfade
 
 React Scrollfade is a React component designed to dynamically fade out the bottom of a scrollable element based on the scrollbars position.
 
 ![demo](./scroll_fade_example.png)
 
-## Installation ##
+## Installation
 
 You can install the library using npm:
 
@@ -12,17 +12,17 @@ You can install the library using npm:
 npm install @benestudioco/react-scrollfade
 ```
 
-## How to use it ##
+## How to use it
 
 It works with zero configuration needed. Just include it as a direct child of the scrollable element.
 The fade makes the bottom of the element transparent, so the color of the fade is determined by what is under it.
 
 ```jsx
-import React from "react";
-import ScrollFade from "@benestudioco/react-scrollfade";
+import React from 'react';
+import ScrollFade from '@benestudioco/react-scrollfade';
 
-export const Example = () =>
- <div
+export const Example = () => (
+    <div
         style={{
             width: 200,
             height: 100,
@@ -31,23 +31,48 @@ export const Example = () =>
         }}
     >
         <ScrollFade />
-        Dolor amet eu occaecat excepteur do adipisicing cillum duis. Est ullamco ullamco sit ea irure consequat
-        id do ex enim consectetur nisi duis elit. Qui mollit magna exercitation est sit.
+        Dolor amet eu occaecat excepteur do adipisicing cillum duis. Est ullamco ullamco sit ea irure consequat id do ex
+        enim consectetur nisi duis elit. Qui mollit magna exercitation est sit.
     </div>
+);
 ```
 
-### Browser Support ###
+### Custom Height
+
+You can also set a different height for the fade
+
+```jsx
+import React from 'react';
+import ScrollFade from '@benestudioco/react-scrollfade';
+
+export const ExampleWithCustomHeight = () => (
+    <div
+        style={{
+            width: 200,
+            height: 100,
+            overflowY: 'auto',
+            background: 'red',
+        }}
+    >
+        <ScrollFade height={30} />
+        Dolor amet eu occaecat excepteur do adipisicing cillum duis. Est ullamco ullamco sit ea irure consequat id do ex
+        enim consectetur nisi duis elit. Qui mollit magna exercitation est sit.
+    </div>
+);
+```
+
+### Browser Support
 
 The component utilizes CSS masks and -webkit-masks as a fallbak.
-See browser compatibilities here: https://caniuse.com/?search=mask 
+See browser compatibilities here: https://caniuse.com/?search=mask
 
-### Examples ###
+### Examples
 
 There are interactive examples.
 
-``` npm run examples ``` starts an instance where you can look at them.
+`npm run examples` starts an instance where you can look at them.
 
-## Contribution ##
+## Contribution
 
 We welcome contributions in the form of pull requests.
 The examples also serve as a development server.
---
+++ examples/WithHeightProp.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ScrollFade from '../lib/ScrollFade';
+
+const WithHeightProp = () => (
+    <div>
+        <div
+            style={{
+                width: 500,
+                position: 'relative',
+                height: 300,
+                overflowY: 'auto',
+                background: 'red',
+            }}
+        >
+            <ScrollFade height={220} />
+            Dolor amet eu occaecat excepteur do adipisicing cillum duis. Est ullamco ullamco sit ea irure consequat id
+            do ex enim consectetur nisi duis elit. Qui mollit magna exercitation est sit. ​ Magna nostrud anim fugiat
+            aliquip consectetur excepteur nostrud voluptate magna reprehenderit magna sunt. Nostrud do sint consequat
+            sunt occaecat Lorem proident nulla. Lorem nisi do culpa nisi elit. Deserunt incididunt anim exercitation
+            cupidatat tempor non minim duis fugiat eiusmod qui mollit do. Ut ea esse proident ex dolore esse officia
+            nulla enim nostrud irure nulla ipsum. Exercitation proident incididunt ea quis tempor Lorem eu officia
+            excepteur aliqua ea dolore dolor id. ​ Ex cillum sunt sunt eiusmod cupidatat irure veniam ullamco commodo
+            qui tempor. Ea ullamco consequat fugiat cillum laborum consectetur velit voluptate eu sint eiusmod
+            exercitation commodo. Consequat elit cupidatat ut eiusmod velit aliquip esse deserunt occaecat sint sunt
+            reprehenderit. Tempor aliqua cillum in in. Dolor nostrud adipisicing in ea esse voluptate incididunt. ​ Sit
+            ut Lorem consectetur ex. Qui reprehenderit nisi veniam elit aliqua commodo enim. Proident pariatur id
+            officia dolor nostrud deserunt do sint in excepteur reprehenderit nostrud exercitation cupidatat. Qui fugiat
+            reprehenderit enim ex nulla do ad amet aliqua deserunt. Aliquip adipisicing amet anim id labore excepteur eu
+            duis occaecat qui sunt amet incididunt cillum. ​ Sit incididunt mollit aliqua eiusmod nulla irure sit nisi
+            enim exercitation elit. Culpa in quis amet excepteur eiusmod duis proident eiusmod ut. Qui quis officia
+            dolore reprehenderit enim aliqua laboris. ​ Consectetur culpa magna voluptate occaecat esse duis quis est.
+            Duis exercitation anim velit proident enim eu culpa ullamco magna magna ullamco in consequat minim. Aliquip
+            ut ad anim et consequat deserunt. Laborum ex et irure culpa irure ea. Tempor aliquip dolore ea veniam
+            excepteur magna ipsum pariatur adipisicing quis qui amet. Dolor exercitation in Lorem labore laborum
+            adipisicing. Laborum consequat ex officia quis Lorem proident ut ullamco.
+        </div>
+    </div>
+);
+
+export default WithHeightProp;
